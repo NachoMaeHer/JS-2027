d8e37d47cae4e7b3cd8ef8277ac8e4361e6c098b
NachoMaeHer <ignacio.maeher@educa.jcyl.es>
2026-09-15 08:41:44 +0200

    Corregir un detalle en en if del bbucle for

--- UT1/PruebaError/script.js
+++ UT1/PruebaError/script.js
@@ -2,5 +2,5 @@ const MAX = 10;
 let seguir = false;
 for (let i = 0; seguir == false; i++) {
   document.body.innerHTML += "<p>" + i + "<p>";
-  if ((i = MAX)) seguir = false;
+  if (i == MAX) seguir = false;
 }
