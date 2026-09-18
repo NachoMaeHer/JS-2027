8e9db05b9844ad065f63c7995bfa4670199f8159
NachoMaeHer <ignacio.maeher@educa.jcyl.es>
2026-09-18 13:09:23 +0200

    Corregir script del ejer 2

--- Js/ActividadesEntrgablesIgnacio/ACT-1/Ej.2/script.js
+++ Js/ActividadesEntrgablesIgnacio/ACT-1/Ej.2/script.js
@@ -1,6 +1,4 @@
 console.log("Ignacio Maestro Heredia");
-const max = 100;
-for (let i = 1; i < max; ++i) {
-  document.body.innerHTML += "<p>" + i + "</p>";
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
 }
-console.log(i);
